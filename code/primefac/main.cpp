db61e7eb72242bb91409f29b0b0cf0130fd131cb
fkoehler <fabian2804@googlemail.com>
2014-05-01 23:01:59 +0200

    Added command line parameters

--- code/primefac/main.cpp
+++ code/primefac/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <climits>
+#include <sstream>
+#include <string>
 using namespace primefac;
 using namespace std;
 
@@ -13,7 +16,11 @@ typedef struct {
 	size_t b1;
 } Step;
 
-int main() {
+void usage();
+
+int main(int argc, char** argv)
+{
+	usage();
 	// Simulation parameters
 	size_t N  = 100;
 	size_t Na = 1000;
@@ -23,13 +30,73 @@ int main() {
 
 	size_t size = sizeof(size_t)*8;
 
-	// Bitsets
 	Bitset Nbit(size, N);
 	size_t n = Nbit.getRelevant();
 
 	vector<Step> steps;
 	Step step = {0, 0, 0, 0};
 	size_t stepCount = 0;
+
+	size_t a_cur = 0;
+	size_t b_cur = 0;
+	size_t a1_cur = 0;
+	size_t b1_cur = 0;
+
+	double T = 1.0;
+	size_t compliance = 0;
+	size_t complianceNew = 0;
+
+	Bitset A(size);
+	Bitset B(size);
+	Bitset Anew(size);
+	Bitset Bnew(size);
+	Bitset prod(size, n);
+
+	mt19937 gen;
+	uniform_int_distribution<short> choiceDist(0, 1);
+	uniform_real_distribution<double> acceptDist(0.0, 1.0);
+
+	// parse command line arguments
+	for(int arg = 1; arg < argc; arg++) {
+		string tmp(argv[arg]);
+
+		if(tmp == string("-N")) {
+			if(argc == argc-1) {
+				usage();
+				return EXIT_FAILURE;
+			}
+			arg++;
+			stringstream(argv[arg]) >> N;
+		} else if(tmp == string("-k")) {
+			if(argc == argc-1) {
+				usage();
+				return EXIT_FAILURE;
+			}
+			arg++;
+			stringstream(argv[arg]) >> kB;
+		} else if(tmp == string("-Na")) {
+			if(argc == argc-1) {
+				usage();
+				return EXIT_FAILURE;
+			}
+			arg++;
+			stringstream(argv[arg]) >> Na;
+		} else if(tmp == string("-Nc")) {
+			if(argc == argc-1) {
+				usage();
+				return EXIT_FAILURE;
+			}
+			arg++;
+			stringstream(argv[arg]) >> Nc;
+		} else if(tmp == string("-Fc")) {
+			if(argc == argc-1) {
+				usage();
+				return EXIT_FAILURE;
+			}
+			arg++;
+			stringstream(argv[arg]) >> Fc;
+		}
+	}
 
 	for(size_t a = n/2; a <= n; a++) {
 		step.a = a;
@@ -46,26 +113,6 @@ int main() {
 		}
 	}
 	stepCount = steps.size();
-
-	size_t a_cur = 0;
-	size_t b_cur = 0;
-	size_t a1_cur = 0;
-	size_t b1_cur = 0;
-
-	double T = 1.0;
-	size_t compliance = 0;
-	size_t complianceNew = 0;
-	size_t complianceTarget = Nbit.quadraticCompliance(Nbit);
-
-	Bitset A(size);
-	Bitset B(size);
-	Bitset Anew(size);
-	Bitset Bnew(size);
-	Bitset prod(size, n);
-
-	mt19937 gen;
-	uniform_int_distribution<short> choiceDist(0, 1);
-	uniform_real_distribution<double> acceptDist(0.0, 1.0);
 
 	for(size_t i = 0; i < stepCount; i++) {
 		cout << steps[i].a << " " << steps[i].b << endl;
@@ -140,3 +187,17 @@ int main() {
 	return 0;
 }
 
+void usage()
+{
+	cout << "primefac - factorize numbers using simulated annealing" << endl;
+	cout << endl;
+	cout << "Usage:" << endl;
+	cout << "  primefac [options]" << endl;
+	cout << endl;
+	cout << "Options:" << endl;
+	cout << "  -N [value]\tThe number to factor, must be larger than 1 and smaller than " << ULONG_MAX << endl;
+	cout << "  -k [value]\tValue for the Boltzmann constant" << endl;
+	cout << "  -Na [value]\tNumber of annealing steps"  << endl;
+	cout << "  -Nc [value]\tNumber of conifgurations per annealing step" << endl;
+	cout << "  -Fc [value]\tCooling factor per annealing step" << endl;
+}
