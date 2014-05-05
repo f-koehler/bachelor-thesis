316698f84b393d8e42e65093abedf962059d6f47
fkoehler <fabian2804@googlemail.com>
2014-05-05 23:01:15 +0200

    added primefac.hpp

---
+++ code/primefac/primefac.hpp
@@ -0,0 +1,44 @@
+#ifndef PRIMEFAC_HPP_
+#define PRIMEFAC_HPP_
+
+#include <vector>
+#include <chrono>
+
+namespace primefac
+{
+	typedef struct {
+		std::size_t N;
+		std::size_t Nc;
+		std::size_t Na;
+		double Fc;
+		double kB;
+	} PrimefacParameters;
+
+	typedef struct {
+		bool success;
+		std::vector<std::size_t> factors;
+		std::chrono::microseconds duration;
+	} PrimefacResult;
+
+	typedef struct {
+		std::size_t N1;
+		std::size_t N2;
+		std::size_t Nc;
+		std::size_t Na;
+		double Fc;
+		double kB;
+	} SemiprimeParameters;
+
+	typedef struct {
+		bool success;
+		std::vector<std::size_t> factors;
+		std::chrono::microseconds duration;
+	} SemiprimeResult;
+
+	PrimefacResult factorize(const PrimefacParameters& parameters);
+	PrimefacResult factorize(const PrimefacParameters& parameters, std::size_t numThreads);
+	SemiprimeResult factorizeSemiprime(const SemiprimeParameters& parameters);
+	SemiprimeResult factorizeSemiprime(const SemiprimeParameters& parameters, std::size_t numThreads);
+}
+
+#endif
