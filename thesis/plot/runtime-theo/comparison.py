0832adafaa85525634019b09e2419349768257e5
fkoehler <fabian2804@googlemail.com>
2014-06-16 22:12:44 +0200

    some corrections in various places

--- thesis/plot/runtime-theo/comparison.py
+++ thesis/plot/runtime-theo/comparison.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 n, r1, r2 = np.loadtxt("data/runtime-theo/data.txt", unpack=True)
 
 plt.plot(n, 1-r2/r1)
+plt.xlim([0, 260])
+plt.xticks([0+i*16 for i in range(0, 17)])
 plt.grid()
 plt.xlabel("$n$")
 plt.ylabel("$1-\\frac{R_2\\left(n\\right)}{R_1\\left(n\\right)}$")
--- thesis/plot/runtime-theo/runtime.py
+++ thesis/plot/runtime-theo/runtime.py
@@ -5,6 +5,8 @@ n, r1, r2 = np.loadtxt("data/runtime-theo/data.txt", unpack=True)
 
 plt.plot(n, r1/n**3, label="$R_1\\left(n\\right)$")
 plt.plot(n, r2/n**3, label="$R_2\\left(n\\right)$")
+plt.xlim([0, 260])
+plt.xticks([0+i*16 for i in range(0, 17)])
 plt.grid()
 plt.xlabel("$n$")
 plt.ylabel("$\\frac{R\\left(n\\right)}{n^3 N_a N_c}$")
