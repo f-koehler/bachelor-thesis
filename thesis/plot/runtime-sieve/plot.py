10daff615ddd1fd416484c3028cdde28fe1777ff
fkoehler <fabian2804@googlemail.com>
2014-06-22 23:47:15 +0200

    kay's corrections

--- thesis/plot/runtime-sieve/plot.py
+++ thesis/plot/runtime-sieve/plot.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 
 n, r1, r2 = np.loadtxt("data/runtime-sieve/data.txt", unpack=True)
 
-plt.plot(n, r1, label="Zahlkörpersieb")
-plt.plot(n, r2, label="Simulated Annealing")
+plt.plot(n, r1, label="Simulated Annealing")
+plt.plot(n, r2, label="Zahlkörpersieb")
 plt.title("$c=64/9$, $N_a=1000$ und $N_c=2\cdot 10^6$")
 plt.yscale("log")
 plt.grid()
